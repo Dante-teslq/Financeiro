97640aae539c6f60578b9a16dc66ce05174699ac
Jose Lucas <joselucasms07@gmail.com>
2024-04-04 12:15:43 -0300

    Adicionei um arquivo de peristência mais um projeto de cria tabelas

--- src/main/java/com/agespisa/servlet/model/Lancamento.java
+++ src/main/java/com/agespisa/servlet/model/Lancamento.java
@@ -4,6 +4,19 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+@Entity
+@Table (name = "Lacamento")
 public class Lancamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -15,7 +28,8 @@ public class Lancamento implements Serializable {
 	private TipoLancamento tipo;
 	private Date dataVencimento;
 	private Date dataPagamento;
-
+	@Id
+	@GeneratedValue
 	public Long getId() {
 		return id;
 	}
@@ -23,7 +37,8 @@ public class Lancamento implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	@ManyToOne (optional = false)
+	@JoinColumn (name = "pessoa-id")
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
@@ -31,7 +46,7 @@ public class Lancamento implements Serializable {
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
-
+	@Column (length = 80, nullable = false)
 	public String getDescricao() {
 		return descricao;
 	}
@@ -39,7 +54,7 @@ public class Lancamento implements Serializable {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
+	@Column (precision = 10, scale = 2, nullable = false)
 	public BigDecimal getValor() {
 		return valor;
 	}
@@ -47,7 +62,8 @@ public class Lancamento implements Serializable {
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
 	}
-
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	public TipoLancamento getTipo() {
 		return tipo;
 	}
@@ -55,7 +71,8 @@ public class Lancamento implements Serializable {
 	public void setTipo(TipoLancamento tipo) {
 		this.tipo = tipo;
 	}
-
+	@Temporal(TemporalType.DATE)
+	@Column (name = "data_vencimento", nullable = false)
 	public Date getDataVencimento() {
 		return dataVencimento;
 	}
@@ -63,7 +80,8 @@ public class Lancamento implements Serializable {
 	public void setDataVencimento(Date dataVencimento) {
 		this.dataVencimento = dataVencimento;
 	}
-
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_pagamento",nullable = true)
 	public Date getDataPagamento() {
 		return dataPagamento;
 	}
@@ -72,4 +90,29 @@ public class Lancamento implements Serializable {
 		this.dataPagamento = dataPagamento;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Lancamento other = (Lancamento) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 }

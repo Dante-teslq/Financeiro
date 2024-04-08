eadbef01bdeb5b9f878e2c4bb7b80f5d3dc2b556
Jose Lucas <joselucasms07@gmail.com>
2024-04-08 12:20:20 -0300

    Paginas web com saidas de texto usando novas bibliotecas

---
+++ src/main/java/com/agespisa/servlet/pagina/CadastroTorcedorBean.java
@@ -0,0 +1,14 @@
+package com.agespisa.servlet.pagina;
+
+public class CadastroTorcedorBean {
+
+	private String timeFavorito;
+	
+	public String timeFavorito() {
+		return timeFavorito;
+	}
+
+	public void setTimeFavorito(String timeFavorito) {
+		this.timeFavorito = timeFavorito;
+	}
+}

06e1c52299997772117372cda16d8003712cb5b6
Jose Lucas <joselucasms07@gmail.com>
2024-04-09 12:04:06 -0300

    Adicionei algumas interfaces,como caixas de escolha e opções de marcação

--- src/main/java/com/agespisa/servlet/pagina/CadastroTorcedorBean.java
+++ src/main/java/com/agespisa/servlet/pagina/CadastroTorcedorBean.java
@@ -3,12 +3,25 @@ package com.agespisa.servlet.pagina;
 public class CadastroTorcedorBean {
 
 	private String timeFavorito;
+	private String termoAceito;
 	
-	public String timeFavorito() {
+	public String getTimeFavorito() {
 		return timeFavorito;
 	}
 
 	public void setTimeFavorito(String timeFavorito) {
 		this.timeFavorito = timeFavorito;
+	}
+	
+	
+	
+	
+	
+	public String getTermoAceito() {
+		return termoAceito;
+	}
+	
+	public void setTermoAceito(String termoAceito) {
+		this.termoAceito = termoAceito;
 	}
 }
